3a8c52e3ec11d619b286950687ed0d15c50845d9
Sajjad <haq.sajjad220@gmail.com>
2025-05-10 02:15:07 -0500

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo: React.FC = () => {
   return (
     <div className="w-12 h-12">
       <img
-        src="https://drive.google.com/drive-viewer/AKGpihbBUXc2NkXhiKE6QciFblD_gS6AGRHlvfO-ZSARmP00u1ch5tnhMr36snxULIFZEXwrTDBTLRI04slvz7vsFCfDFNm1sz_GNg=w1920-h889"
+        src="https://i.imgur.com/HnWLnNR.png"
         alt="Ceramic Catalysts Logo"
         className="w-full h-full object-contain"
       />
